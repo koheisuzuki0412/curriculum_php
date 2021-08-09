10f094cda43f138370a1c32a688afe2401804ace
“ko-suzuki-YF3” <ko-suzuki@y-i-group.co.jp>
2021-08-10 07:15:11 +0900

    PHP2-11
    フォームに入力した内容を〜

--- 2-11/index.php
+++ 2-11/index.php
@@ -1,5 +1,9 @@
-<?php
-  // <form action = "result.php" method = "get">
-  // 名前：<input type = "text" name = "my_name" />
-  <p></p>
-?>
+<form action="result.php" method="post">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス：<input type = "text" name = "email">
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
--- 2-11/result.php
+++ 2-11/result.php
@@ -1,6 +1,8 @@
 <?php
-$my_name = $_GET['my_name'];
+$my_name = $_POST['my_name'];
+$email = $_POST['email'];
 $password = $_GET['password'];
 ?>
 <p>私の名前は、<?php echo $my_name; ?></p>
+<p>メールアドレスは、<?php echo $email; ?></p>
 <p>私のパスワードは、<?php echo $password; ?></p>
